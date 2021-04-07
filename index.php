cc72959891fb11f6d7b5aa05de4ecacfcd9b1ae1
CaseyNorth <Caseynorth182@gmail.com>
2021-04-07 23:20:05 +0300

    add slim framework

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+//режим строгой типизации
+declare(strict_types=1);
+
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+$app->get('/about', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("about page");
+    return $response;
+});
+
+$app->run();
